57af1ff04ab1829465707a8d10027b08adb751f1
minji <my970524@naver.com>
2022-07-12 21:50:49 +0900

    Chore : config/settings.py 수정
    - APPEND_SLASH=False 추가
    - 한국 시간 사용을 위해 셋팅 수정

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+APPEND_SLASH = False
+
 ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "user.User"  # app.table 형태
@@ -105,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
