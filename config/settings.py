51ed3b1ba33388928e00683d7dc1e9355ce1acb5
김훈희 <nmdkims@gmail.com>
2022-07-12 11:20:04 +0900

    Feat : #13 로그인 기능 추가
    - 잘못된 패키지 수정 django-restframework -> djangorestframework
    - settings.py 앱 추가 내용 위 변경 내용으로 반영
    - UserApiView 추가 (로그인 기능)

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_restframework",
+    "rest_framework",
     "boss_raid",
     "raid_record",
     "user",
