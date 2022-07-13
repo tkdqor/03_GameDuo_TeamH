2da828ef20a6c57e1237c30cea6d04ab583d5deb
sangbaek <aumsbk@naver.com>
2022-07-13 20:53:09 +0900

    Fix : 충돌 수정

--- user/user_tests/user_api_tests.py
+++ user/user_tests/user_api_tests.py
@@ -1,4 +1,4 @@
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIClient, APITestCase
 
 from user.models import User
 
@@ -20,23 +20,23 @@ class UserSiginUpViewTestCase(APITestCase):
             "nickname": "sangbaek",
             "password": "1234",
         }
-        response = self.client.post(self.url, data=user_data, format='json')
+        response = self.client.post(self.url, data=user_data, format="json")
         self.assertEqual(200, response.status_code)
-    
+
     def test_unique_nickname_validation(self):
         """nickname 중복 여부 테스트"""
         user_data_1 = {
             "nickname": "sangbaek2",
             "password": "1234",
         }
-        response = self.client.post(self.url, data=user_data_1, format='json')
+        response = self.client.post(self.url, data=user_data_1, format="json")
         self.assertEqual(200, response.status_code)
 
         user_data_2 = {
             "nickname": "sangbaek2",
             "password": "1234",
         }
-        response = self.client.post(self.url, data=user_data_2, format='json')
+        response = self.client.post(self.url, data=user_data_2, format="json")
         self.assertEqual(400, response.status_code)
 
 
@@ -55,7 +55,7 @@ class UserSignInViewTestCase(APITestCase):
         self.nickname = "sangbaek"
         self.password = "1234"
         self.user = User.objects.create_user(self.nickname, self.password)
-    
+
     def test_authentication(self):
         """로그인 테스트"""
         response = self.client.post(self.url, {"nickname": self.nickname, "password": self.password})
@@ -76,7 +76,7 @@ class UserLookupTestCase(APITestCase):
     """
     Assignee : 상백
 
-    회원 전체 조회 테스트입니다. 
+    회원 전체 조회 테스트입니다.
     setUp 메서드로 로그인 상태를 설정 후 회원 전체 조회 여부를 확인합니다.
     """
 
@@ -89,7 +89,7 @@ class UserLookupTestCase(APITestCase):
         self.user = User.objects.create_user(**payload)
         self.client = APIClient()
         self.client.force_authenticate(self.user)
-    
+
     def test_user_api_view_get(self):
         """회원 전체 조회 테스트"""
         url = "/user/"
