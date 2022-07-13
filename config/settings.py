b50376bc94c402be09c91e67e0f124ae3c000ed4
Github <weeds1590@gmail.com>
2022-07-13 14:33:46 +0900

    Add : feature/#3 mysql 사용을 위한 세팅 추가
    - mysql client 패키지 설치
    - docker-compose mysql 옵션 추가

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from .my_settings import SECRET_KEY
+from .my_settings import DATABASES, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +78,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
