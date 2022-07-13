af25e84059a3f73124601977f2d4149877646d9b
김훈희 <nmdkims@gmail.com>
2022-07-14 01:00:58 +0900

    Add : jwt_claim_serializer 추가
    - claim에 id 와 nickname이 같이 실릴 수 있게 설정

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import UserListAPIView, UserSigninApiView, UserSignupApiView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("signin", UserSigninApiView.as_view()),
     path("signup", UserSignupApiView.as_view()),
     path("", UserListAPIView.as_view()),
