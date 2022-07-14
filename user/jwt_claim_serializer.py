b2df9026256a5283fa314c012c5803a4f17503a1
김훈희 <nmdkims@gmail.com>
2022-07-15 00:44:29 +0900

    Refactor : #37 로그인시 토큰 같이 발급 받는 기능 추가
    - 로그인 기능을 수행하면 토큰을 함께 전달
    - 사용된 GameTokenObtainPairSerializer은 custom된 내용으로
    payload data에 닉네임과 id를 담고 있음

--- user/jwt_claim_serializer.py
+++ user/jwt_claim_serializer.py
@@ -1,4 +1,7 @@
+from django.utils.text import gettext_lazy as _
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 
 class GameTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -19,3 +22,19 @@ class GameTokenObtainPairSerializer(TokenObtainPairSerializer):
         token["nickname"] = user.nickname
 
         return token
+
+
+class RefreshTokenSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_messages = {"bad_token": _("Token is invalid or expired")}
+
+    def validate(self, attrs):
+        self.token = attrs["refresh"]
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail("bad_token")
--- user/views.py
+++ user/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from user.jwt_claim_serializer import GameTokenObtainPairSerializer
+from user.jwt_claim_serializer import GameTokenObtainPairSerializer, RefreshTokenSerializer
 from user.models import User as UserModel
 from user.serializers import UserListDetailSerializer, UserListSerializer, UserSigninSerializer, UserSignupSerializer
 
@@ -15,7 +16,7 @@ class UserSignupApiView(APIView):
     """
     Assignee : 훈희
 
-    회원가입 view 입니다.
+    post : 회원가입
     회원가입시 입력 data 타입 json 구조는 밑과 같습니다.
     {
         "nickname" : "test1",
@@ -35,22 +36,26 @@ class UserSignupApiView(APIView):
             return Response({"messages": "가입 실패"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# /users/signin
-class UserSigninApiView(APIView):
+# /users/login
+class LoginView(APIView):
     """
     Assignee : 훈희
 
-    로그인과 회원 전체 조회, 회원 단건 조회 기능입니다.
+    post : 로그인
+    로그인 할때 access token과 refresh token을 함께 가져옴
     로그인시 입력 data 타입 json 구조는 밑과 같습니다.
     {
         "nickname" : "test1",
         "password" : "root1234"
     }
 
+    delete : 로그아웃
+    테스트등 개발 환경에서 사용하기 위한 간단한 로그아웃 기능
+    토큰을 반납하지 않고 로그아웃이 됩니다.
+
     """
 
     permission_classes = [AllowAny]
-    user_serializer = UserSigninSerializer
 
     def post(self, request):
         nickname = request.data.get("nickname", "")
@@ -60,55 +65,87 @@ class UserSigninApiView(APIView):
         if not user:
             return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
+        user_serializer = UserSigninSerializer(user)
+        token = GameTokenObtainPairSerializer.get_token(user)
+        refresh_token = str(token)
+        access_token = str(token.access_token)
+        response = Response(
+            {
+                "user": user_serializer.data,
+                "message": "로그인 성공!!",
+                "token": {
+                    "access": access_token,
+                    "refresh": refresh_token,
+                },
+            },
+            status=status.HTTP_200_OK,
+        )
         login(request, user)
-        return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
+        return response
 
     def delete(self, request):
         user = request.user
         logout(request)
-        return Response(f"로그아웃 되었습니다.{user}님 안녕히가세요!")
+        return Response(f"토큰을 유지하고 로그아웃 되었습니다.{user}님 안녕히가세요!")
+
+
+# /users/logout
+class LogoutView(GenericAPIView):
+    serializer_class = RefreshTokenSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args):
+        refresh = self.get_serializer(data=request.data)
+        refresh.is_valid(raise_exception=True)
+        refresh.save()
+
+        user = request.user
+        logout(request)
+
+        return Response(f"토큰을 반납하고 로그아웃 되었습니다.{user}님 안녕히가세요!", status=status.HTTP_204_NO_CONTENT)
 
 
 # /users/
 class UserListAPIView(APIView):
+    """
+    Assignee : 훈희
+
+    get : 회원 전체 조회
+    플레이어 전체 목록이 나옵니다.
+    해당 내용은 admin 유저만 확인 가능합니다.
+
+    """
+
     permission_classes = [IsAdminUser]
     user_serializer = UserListSerializer
 
     def get(self, request):
-        """
-        Assignee : 훈희
-
-        플레이어 전체 목록이 나옵니다.
-        해당 내용은 admin 유저만 확인 가능합니다.
-
-        """
         all_user = UserModel.objects.all().order_by("last_login")
         serializer = UserSigninSerializer(all_user, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-# /api/gametoken
+# /users/api/gametoken
 class GameTokenObtainPairView(TokenObtainPairView):
+    """
+    Assignee : 훈희
+
+    커스텀 된 토큰 시리얼라이저 테스트용 View입니다.
+
+    """
+
     serializer_class = GameTokenObtainPairSerializer
 
 
+# /users/<user_id>
 class UserListDetailAPIView(APIView):
     """
     Assignee : 훈희
 
-    permission = 모두 가능
-    Http method = GET
-    GET : 유저 단건 조회
-
-    response
-    {
-        totalScore:number,
-            bossRaidHistory: [
-            { raidRecordId:number, score:number, enterTime:string, endTime:string },
-            //..
-        ]
-    }
+    get : 유저 단건 조회
+    totalScore와 bossRaidHistory가 표시되는 유저 단건 조회 입니다.
+    밑의 response 구조에 맞춘 유저 단건 조회 입니다.
 
     """
 
